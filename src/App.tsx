751390669263611e443bfe3c5f61c51b8ef4c27f
edobxt <kanjo.perso@gmail.com>
2022-04-27 12:38:22 -0400

    Premier commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const calcul: any = (premierNombre: string, operateur: string, secondNombre: string) => {
+    return eval("(" + premierNombre + operateur + secondNombre + ")");
+}
+
+const App = () => {
+  const [premierNombre, setPremierNombre] = useState("");
+  const [secondNombre, setSecondNombre] = useState("");
+  const [operateur, setOperateur] = useState("+");
+  const [resultat, setResultat] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>State Calculator </h1>
+        <label htmlFor={"premierNombre"}>Premier nombre : </label>
+        <input
+            type="text" id={"premierNombre"} onChange={event => setPremierNombre(event.target.value)}/>
+        <br/>
+        <select id={"operateur"} onChange={event => setOperateur(event.target.value)}>
+            <option value="+">➕</option>
+            <option value="-">➖</option>
+            <option value="*">✖️</option>
+            <option value="/">➗</option>
+        </select>
+        <br/>
+        <label htmlFor={"secondNombre"}>Second nombre : </label>
+        <input
+            type="text" id={"secondNombre"}
+            onChange={event => setSecondNombre(event.target.value)}
+        />
+        <br/>
+        <button onClick={() => setResultat(calcul(premierNombre, operateur, secondNombre))}
+              id={"bouton-egale"} type={"button"}>
+            =
+        </button>
+        <span id={"resultat"}>
+            {resultat}
+        </span>
     </div>
   );
 }
